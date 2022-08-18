6f0063cbca37725c512ba25a3aa8b747e7226c5d
Nilufa Khan <khannilufa5@gmail.com>
2022-08-18 21:08:41 +0530

    Inittial commit

---
+++ src/derived/Petrol.java
@@ -0,0 +1,10 @@
+package derived;
+
+import data.IcEngine;
+
+public class Petrol extends IcEngine {
+    public Petrol(int modelNo, int maxPower, int maxRPM, int displacement, int noOfCyllender, String fuelType) {
+        super(modelNo, maxPower, maxRPM, displacement, noOfCyllender, fuelType);
+    }
+
+}
